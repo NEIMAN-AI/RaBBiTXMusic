f78b29231c7fc66a5e111634861cb8835689c3cc
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-15 08:12:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,22 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# API_IDS
+API_ID = int(getenv("API_ID")) # API_ID get it from my.telegram.org
+API_HASH = getenv("API_HASH") # API_HASH get it from my.telegram.org
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-
+# SESSIONS
 BOT_TOKEN = getenv("BOT_TOKEN", None)
+SESSION = getenv("SESSION", None)
+
+# LIMITS
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
+# OWNER'S
 OWNER_ID = int(getenv("OWNER_ID"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
+# IMAGES
 PING_IMG = getenv("PING_IMG", None)
 START_IMG = getenv("START_IMG", None)
 FAILED = getenv("FAILED", None)
-SESSION = getenv("SESSION", None)
 
+# SUPPORTS
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", None)
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", None)
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 LOGGER_ID = getenv("LOGGER_ID", None)
