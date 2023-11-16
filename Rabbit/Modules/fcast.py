7d1a9b6bf169db6333aeaa474d4b19c53cd7c70c
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-16 10:15:51 +0530

    Create fcast.py

---
+++ Rabbit/Modules/fcast.py
@@ -0,0 +1,8 @@
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import filters, Client, errors, enums
+from pyrogram.errors import UserNotParticipant
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from Powers.database import add_user, add_group, all_users, all_groups, users, remove_user
+from Rabbit import app
+from config import SUDO_USERS
+import config
