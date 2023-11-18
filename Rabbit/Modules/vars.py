2fb14fc10c6c4e78e84d1280a5b30605128dcda0
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-18 11:06:34 +0530

    Update vars.py

--- Rabbit/Modules/vars.py
+++ Rabbit/Modules/vars.py
@@ -4,6 +4,7 @@ from pyrogram.types import Message
 
 import config
 from Rabbit import BOT_NAME, app
+from Rabbit import BUNNY_NAME
 
 
 @app.on_message(
@@ -13,7 +14,7 @@ async def get_vars(_, message: Message):
     try:
         await app.send_message(
             chat_id=int(config.OWNER_ID),
-            text=f"""<u>**{BOT_NAME} ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs :**</u>
+            text=f"""<u>**{BOT_NAME} & {BUNNY_NAME} ᴄᴏɴғɪɢ ᴠᴀʀs :**</u>
 
 **ᴀᴘɪ_ɪᴅ :** `{config.API_ID}`
 **ᴀᴘɪ_ʜᴀsʜ :** `{config.API_HASH}`
@@ -25,18 +26,23 @@ async def get_vars(_, message: Message):
 **sᴜᴅᴏ_ᴜsᴇʀs :** `{config.SUDO_USERS}`
 
 **ᴘɪɴɢ_ɪᴍɢ :** `{config.PING_IMG}`
-**ᴀʟɪᴠᴇ_ɪᴍɢ :** `{config.ALIVE_IMG}`
+**FAILED :** `{config.FAILED}`
 **sᴛᴀʀᴛ_ɪᴍɢ :** `{config.START_IMG}`
 
 **sᴜᴘᴘᴏʀᴛ_ᴄʜᴀᴛ :** `{config.SUPPORT_CHAT}`
 **sᴜᴘᴘᴏʀᴛ_ᴄʜᴀɴɴᴇʟ :** `{config.SUPPORT_CHANNEL}`
 
+**ʟᴏɢ_ɢʀᴏᴜᴘ_ɪᴅ :** `{config.LOG_GROUP_ID}`
+**ᴄᴏᴍᴍᴀɴᴅ_ᴘʀᴇғɪxᴇs :** `{config.COMMAND_PREFIXES}`
+**ᴍᴏɴɢᴏᴅʙ_ᴜʀʟ :** `{config.MONGODB_URL}`
+
+**ᴘᴍᴘᴇʀᴍɪᴛ :** `{config.PMPERMIT}`
 **sᴇssɪᴏɴ :** `{config.SESSION}`""",
             disable_web_page_preview=True,
         )
     except:
-        return await message.reply_text("» ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs.")
+        return await message.reply_text("» ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴠᴀʀs.")
     if message.chat.type != ChatType.PRIVATE:
         await message.reply_text(
-            "» ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘᴍ, ɪ'ᴠᴇ sᴇɴᴛ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs ᴛʜᴇʀᴇ."
+            "» ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘᴍ, ɪ'ᴠᴇ sᴇɴᴛ ᴛʜᴇ ᴠᴀʀs ᴛʜᴇʀᴇ."
         )
