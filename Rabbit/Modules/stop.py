317fe3964fa1a088b4f55303fe63c7d039ddbaf4
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-18 11:11:41 +0530

    Create stop.py

---
+++ Rabbit/Modules/stop.py
@@ -0,0 +1,24 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Rabbit import app, pytgcalls
+from Rabbit.Powers import _clear_, admin_check, close_key
+
+
+@app.on_message(filters.command(["stop", "end"]) & filters.group)
+@admin_check
+async def stop_str(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        await _clear_(message.chat.id)
+        await pytgcalls.leave_group_call(message.chat.id)
+    except:
+        pass
+
+    return await message.reply_text(
+        text=f"•➢** sᴛʀᴇᴀᴍ sᴛᴏᴩᴩᴇᴅ ʙʏ** » {message.from_user.mention}",
+        reply_markup=close_key,
+    )
