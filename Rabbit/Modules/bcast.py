e77f89896f708486b317c670ec41ef2f4c2bc133
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-18 15:08:24 +0530

    Update bcast.py

--- Rabbit/Modules/bcast.py
+++ Rabbit/Modules/bcast.py
@@ -2,7 +2,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, 
 from pyrogram import filters, Client, errors, enums
 from pyrogram.errors import UserNotParticipant
 from pyrogram.errors.exceptions.flood_420 import FloodWait
-from Powers.database import add_user, add_group, all_users, all_groups, users, remove_user
+from Rabbit.Powers.database import add_user, add_group, all_users, all_groups, users, remove_user
 from Rabbit import app
 from config import SUDO_USERS
 import config
