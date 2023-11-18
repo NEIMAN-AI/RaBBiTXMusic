4aa1920f91527e0889404b251f87cd497ba4d68c
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-18 17:19:24 +0530

    Update __main__.py

--- Rabbit/__main__.py
+++ Rabbit/__main__.py
@@ -42,7 +42,7 @@ async def rabbit_startup():
         )
     except:
         LOGGER.error(
-            f"{BOT_NAME} failed to send message at @{SUNAME}, go & check. logs fast!!"
+            f"{BOT_NAME} failed to send message at , go & check. logs fast!!"
         )
 
     try:
@@ -52,7 +52,7 @@ async def rabbit_startup():
         )
     except:
         LOGGER.error(
-            f"{ASS_NAME} failed to send message at @{SUNAME},  go & check logs fast!!."
+            f"{ASS_NAME} failed to send message at ,  go & check logs fast!!."
         )
 
     await app2.send_message(BOT_USERNAME, "/start")
